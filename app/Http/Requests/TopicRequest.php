cd450340390651b21348a83433912de5f1e64b79
Boris <chenjunchi414@gmail.com>
2021-01-05 08:32:56 +0000

    user create topic

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -10,32 +10,30 @@ class TopicRequest extends Request
         {
             // CREATE
             case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
-            // UPDATE
+                // UPDATE
             case 'PUT':
             case 'PATCH':
             {
                 return [
-                    // UPDATE ROLES
+                    'title'       => 'required|min:2',
+                    'body'        => 'required|min:3',
+                    'category_id' => 'required|numeric',
                 ];
             }
             case 'GET':
             case 'DELETE':
             default:
-            {
-                return [];
-            }
+                {
+                    return [];
+                };
         }
     }
 
     public function messages()
     {
         return [
-            // Validation messages
+            'title.min' => 'Title must be at least two characters',
+            'body.min' => 'Article content must be at least three characters',
         ];
     }
 }
--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,5 +1,7 @@
 <div class="card ">
     <div class="card-body">
-        right navigation
+        <a href="{{ route('topics.create') }}" class="btn btn-success btn-block" aria-label="Left Align">
+            <i class="fas fa-pencil-alt mr-2"></i>  New post
+        </a>
     </div>
 </div>
