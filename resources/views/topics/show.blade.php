ef6d5c362785398883eba4f5f16d799a0232e7be
Boris <chenjunchi414@gmail.com>
2021-01-10 08:57:30 +0000

    reply list

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -11,7 +11,7 @@
             <div class="card ">
                 <div class="card-body">
                     <div class="text-center">
-                        Owner：{{ $topic->user->name }}
+                        作者：{{ $topic->user->name }}
                     </div>
                     <hr>
                     <div class="media">
@@ -26,7 +26,7 @@
         </div>
 
         <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 topic-content">
-            <div class="card ">
+            <div class="card">
                 <div class="card-body">
                     <h1 class="text-center mt-3 mb-3">
                         {{ $topic->title }}
@@ -51,7 +51,7 @@
                             </a>
                             <form action="{{ route('topics.destroy', $topic->id) }}" method="post"
                                   style="display: inline-block;"
-                                  onsubmit="return confirm('您确定要删除吗？');">
+                                  onsubmit="return confirm('Are u sure about this？');">
                                 {{ csrf_field() }}
                                 {{ method_field('DELETE') }}
                                 <button type="submit" class="btn btn-outline-secondary btn-sm">
@@ -63,6 +63,15 @@
 
                 </div>
             </div>
+
+            {{-- 用户回复列表 --}}
+            <div class="card topic-reply mt-4">
+                <div class="card-body">
+                    @include('topics._reply_box', ['topic' => $topic])
+                    @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
+                </div>
+            </div>
+
         </div>
     </div>
 @stop
