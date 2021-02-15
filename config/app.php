7d1200103bc2619179a8cd05dfc0a14397a7e4ea
Boris <chenjunchi414@gmail.com>
2021-02-15 11:08:29 +0000

    push notification

--- config/app.php
+++ config/app.php
@@ -174,7 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\ClickSendServiceProvider::class,
+        App\Providers\ClickSendServiceProvider::class
+        App\Providers\JpushServiceProvider::class,
 
         'Mews\Captcha\CaptchaServiceProvider',
 
